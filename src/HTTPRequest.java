4db51881e6f034122804ca99f215d10ea0e07127
Bar Jakobovitz <barzk98@gmail.com>
2024-02-03 12:39:09 +0200

    HTTPRequest work for example

---
+++ src/HTTPRequest.java
@@ -0,0 +1,38 @@
+import java.util.HashMap;
+
+public class HTTPRequest {
+     String httpRequest;
+     String requestType; 
+     String requestedPage;
+     boolean isImage;
+     int contentLength;
+     String referer;
+     String userAgent; 
+     HashMap<String,String> parameters; 
+
+     public HTTPRequest(String Httprequest)
+     {
+       httpRequest=Httprequest;
+       ParseTheRequest();
+     }
+     public void ParseTheRequest()
+     {
+       String[] lines = this.httpRequest.split("\n");
+       String input=lines[0];
+       //handeling the 1th line, getting the type and page
+       requestType = input.substring(0, input.indexOf(' '));
+       input=input.substring(input.indexOf(' ')+1,input.length());
+       requestedPage = input.substring(0, input.indexOf(' '));
+       isImage = requestedPage.matches(".+\\.(jpg|bmp|gif|jpeg|tiff|psd|svg|raw|ico|heic)$");
+        //handeling the 3th line, getting the Length
+        input=lines[2];
+        int contentLength=Integer.parseInt(input.substring(input.indexOf(' ')+1, input.length()));
+        //handeling the 4th line, getting the referer 
+        input=lines[3];
+        referer=input.substring(input.indexOf(' ')+1, input.length());
+        //handeling the 5th line, getting the user agent
+        input=lines[4];
+        userAgent=input.substring(input.indexOf(' ')+1, input.length());
+       
+     }
+    }
--- src/Main.java
+++ src/Main.java
@@ -2,6 +2,12 @@
 // click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
 public class Main {
     public static void main(String[] args) {
-        System.out.println("First Project1");
+       String requestHeader = "GET /index.html?param1=value1&param2=value2 HTTP/1.1\n" +
+                               "Host: example.com\n" +
+                               "Content-Length: 100\n" +
+                               "Referer: http://referer.com\n" +
+                               "User-Agent: Mozilla/5.0";
+        
+        HTTPRequest httpRequest = new HTTPRequest(requestHeader);
     }
 }
